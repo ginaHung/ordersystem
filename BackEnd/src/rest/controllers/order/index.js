c6b4029fb81f18b8967c8cedb992f81401e9e0f1
10905306 <fangtzu_lin@wistron.com>
2021-05-18 17:34:34 +0800

    update api

--- BackEnd/src/rest/controllers/order/index.js
+++ BackEnd/src/rest/controllers/order/index.js
@@ -31,6 +31,62 @@ const router = new Router();
  */
   router.post('/getMyOrder', controller.getMyOrder);
 
+  /**
+ * @api {POST} /api/order/saveOrder
+ * @apiName addOrder
+ * @apiGroup login
+ * @apiVersion 1.0.0
+ * @apiParam {string} user, who want to login
+ * @apiParam {string} password, user's password
+ * @apiSuccessExample {json} Request Example
+ * {
+ *   id_num: '20210425-1016', 
+ *   name: 'Drink-Milk Shop',
+ *   user_id: '10910305',
+ *   user: 'Gina',
+ *   description: '喝好喝滿',
+ *   endtime: '2021/04/30 12:00',
+ *   invite_code: '123456',
+ *   menu_id: '',
+ *   class_1: '',
+ *   class_2: '',
+ *   class_3: '',
+ *   class_4: '',
+ *   class_5: '',  
+ * }
+ */
+   router.post('/saveOrder', controller.saveOrder);
+
+   /**
+ * @api {POST} /api/order/getOrderData
+ * @apiName getOrderList
+ * @apiGroup login
+ * @apiVersion 1.0.0
+ * @apiParam {string} id, who want to login
+
+ * @apiSuccessExample {json} Request Example
+ * {
+ *   header_id: '1'
+ *  
+ * }
+ */
+    router.post('/getOrderData', controller.getOrderData);
+
+  
+   /**
+ * @api {POST} /api/order/getOrderItem
+ * @apiName getOrderList
+ * @apiGroup login
+ * @apiVersion 1.0.0
+ * @apiParam {string} id, who want to login
+
+ * @apiSuccessExample {json} Request Example
+ * {
+ *   header_id: '1'
+ *  
+ * }
+ */
+    router.post('/getOrderItem', controller.getOrderItem);
 
  /**
  * @api {POST} /api/order/addOrder
